ed60bd0a3e6427c6afb0af5778fbe87b18ad14cd
Francesco Spagnolo <francesco.spagnolo95@gmail.com>
2025-09-19 10:32:26 +0200

    feat: redirect all paths to homepage

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	async redirects() {
+		return [
+			{
+				source: "/((?!$|api|_next/static|_next/image|favicon.ico|fonts|logo.svg).+)",
+				destination: "/",
+				permanent: false,
+			},
+		];
+	},
 };
 
 export default nextConfig;
